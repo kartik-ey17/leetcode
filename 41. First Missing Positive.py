03ffe4dd6a09b92cc827d29b8755d6a35782bec6
kartik-ey17 <kartikey.25bai11235@vitbhopal.ac.in>
2026-09-06 17:17:14 +0530

    first missing positive

---
+++ 41. First Missing Positive.py
@@ -0,0 +1,10 @@
+def firstMissingPositive(nums):
+        n = len(nums)
+        for i in range(n):
+            while 1 <= nums[i] and nums[nums[i] - 1] != nums[i]:
+                  nums[nums[i] - 1] , nums[i] = nums[nums[i] - 1]
+        for i in range(n):
+            if nums[i] != i + 1:
+                  return i + 1
+        return n + 1
+print(firstMissingPositive([1,2,0]))
