8f32fe8e467bd5d39df0851bd70c6e7947c11568
kartik-ey17 <kartikey.25bai11235@vitbhopal.ac.in>
2026-09-07 22:10:54 +0530

    4sum2 O(n^2) lessgoo

--- 4sum2{###}.py
+++ 4sum2{###}.py
@@ -6,14 +6,15 @@ find the number of 4-number combinations whose sum is 0
 no other 4 number combination from the given arrays equate to 0 
 => output is 2
 """
-def foursum(nums1,nums2,nums3,nums4):
-    sol = 0
-    for i in nums1:
-        for j in nums2:
-            for k in nums3:
-                for l in nums4:
-                    if i+j+k+l == 0:
-                        sol += 1
-    return sol
+from collections import defaultdict
+def four_sum(nums1,nums2,nums3,nums4):
+    h_map = defaultdict(int)
+    four_sum = 0
+    for n1 in nums1:
+        for n2 in nums2:
+            h_map[n1 + n2] += 1
 
-print(foursum([1,2] , [-2,-1] , [-1,2] , [0,2]))
+    for n3 in nums3:
+        for n4 in nums4:
+            four_sums += h_map[-(n3+n4)]
+    return four_sum
