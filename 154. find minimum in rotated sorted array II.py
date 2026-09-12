a6355b5adb0399d77fc29b37b7f9848f2d8e52ed
kartik-ey17 <kartikey.25bai11235@vitbhopal.ac.in>
2026-09-12 18:49:41 +0530

    hard but easy?

---
+++ 154. find minimum in rotated sorted array II.py
@@ -0,0 +1,13 @@
+def findMin(nums):
+    l = 0
+    r = len(nums) - 1
+    while l < r:
+        mid = (l+r)//2
+        if nums[mid] < nums[r]:
+            r = mid
+        elif nums[mid] > nums[r]:
+            l = mid+1
+        else:
+            r -= 1
+    return nums[l]
+print(findMin([4,5,6,7,0,0,1,4]))
