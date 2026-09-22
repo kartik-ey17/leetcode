1143c5a3537b5b53853abaa6b9561cdf1361d4dd
kartik-ey17 <kartikey.25bai11235@vitbhopal.ac.in>
2026-09-22 21:37:36 +0530

    backtrakk

---
+++ 46.Permutations.py
@@ -0,0 +1,12 @@
+def permute(nums):
+    result = []
+    def backtrack(start):
+        if len(nums) == start:
+            result.append(nums[:])
+            return
+        for i in range(start , len(nums)):
+            nums[start] , nums[i] = nums[i] , nums[start]
+            backtrack(start+1)
+            nums[start] , nums[i] = nums[i] , nums[start]
+        backtrack(0)
+        return result
